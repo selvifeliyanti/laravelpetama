6f79642c4dd327793b9c2862b154ed9d1fe7d67b
selvifeliyanti <selvifeliyanti099@gmail.com>
2021-01-20 16:34:40 +0700

    commit8

--- app/Http/Controllers/CobaController.php
+++ app/Http/Controllers/CobaController.php
@@ -1,34 +1,20 @@
-<?php
-
-namespace App\Http\Controllers;
-use App\Models\friends;
-use Illuminate\Http\Request;
-
-
-class CobaController extends Controller
-{
-   
-    public function index()
-    {
-        $friends = friends::paginate(3);
-
-        return view('index',compact('friends'));
-    }
-    public function create()
-    {
-        
-        return view('create');
-    }
-    public function store(Request $request)
-    {
-        // Validate the request...
-
-        $friends = new friends;
-
-        $friends->nama = $request->nama;
-        $friends->no_tlpn = $request->no_tlpn;
-        $friends->alamat = $request->alamat;
-
-        $friends->save();
-    }
-}
+<?php	<?php
+use App\Http\Controllers\Api\CobaController;	use App\Http\Controllers\Api\CobaController;
+use App\Http\Controllers\Api\GroupsController;	use App\Http\Controllers\Api\GroupsController;
+use Illuminate\Http\Request;	use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;	use Illuminate\Support\Facades\Route;
+/*	/*
+|--------------------------------------------------------------------------	|--------------------------------------------------------------------------
+| API Routes	| API Routes
+|--------------------------------------------------------------------------	|--------------------------------------------------------------------------
+|	|
+| Here is where you can register API routes for your application. These	| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which	| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!	| is assigned the "api" middleware group. Enjoy building your API!
+|	|
+*/	*/
+route::get('', [CobaController::class, 'index']);	route::get('', [CobaController::class, 'index']);
+route::resources([	route::resources([
+    'friends' =>CobaController::class,	    'friends' =>CobaController::class,
+    'groups' =>groupscontroller::class,	    'groups' =>GroupsController::class,
+]); 	]); 
