56db6316d41093cca1c27898215e20ad95cec484
selvifeliyanti <selvifeliyanti099@gmail.com>
2021-01-28 22:03:31 +0700

    commit 9 rest api show update destory

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,30 @@
 <?php
+			
+		use App\Http\Controllers\Api\CobaController;
+		use App\Http\Controllers\Api\GroupsController;
+		use Illuminate\Http\Request;
+		use Illuminate\Support\Facades\Route;
+		
+		/*
+		|--------------------------------------------------------------------------
+		| API Routes
+		|--------------------------------------------------------------------------
+		|
+		| Here is where you can register API routes for your application. These
+		| routes are loaded by the RouteServiceProvider within a group which
+		| is assigned the "api" middleware group. Enjoy building your API!
+		|
+		*/
+		route::get('/friends/{id}', [Cobacontroller::class, 'show']);
+		route::put('/friends/{id}', [Cobacontroller::class, 'update']);
+		route::delete('/friends/{id}', [Cobacontroller::class, 'destroy']);
+		
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+		route::get('', [CobaController::class, 'index']);
+		route::resources([
+		'friends' =>CobaController::class,
+		'groups' =>GroupsController::class,
+	]); 		]);
+			route::get('/groups/addmember/{group}', [Groupscontroller::class, 'addmember']);
+			route::put('/groups/addmember/{group}', [Groupscontroller::class, 'updateaddmember']);
+			route::put('/groups/delateaddmember/{group}', [Groupscontroller::class, 'delateaddmember']); 
